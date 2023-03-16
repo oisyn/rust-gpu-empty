cb0747658977176b9a44d68ee036cba963a6c2ed
Sylvester Hesp <github@oisyn.nl>
2023-03-16 10:17:13 +0100

    Minimal crash repro

--- shaders/src/lib.rs
+++ shaders/src/lib.rs
@@ -2,12 +2,23 @@
     target_arch = "spirv",
     no_std
 )]
+#![allow(unused_variables)]
 
-use spirv_std::spirv;
+use spirv_std::{spirv, ray_tracing::AccelerationStructure};
+use spirv_std::glam::*;
 
-use spirv_std::glam::{vec4, Vec4};
+#[derive(Clone, Copy)]
+#[repr(C)]
+pub struct PushConstants {
+    pub acceleration_structure_address: u64,
+}
+
 
 #[spirv(fragment)]
-pub fn main_fs(output: &mut Vec4) {
-    *output = vec4(1.0, 0.0, 0.0, 1.0);
+pub fn main_fs(
+    #[spirv(push_constant)] push_constants: &PushConstants,
+    output: &mut Vec4,
+) {
+    let acceleration_structure =
+    unsafe { AccelerationStructure::from_u64(push_constants.acceleration_structure_address) };
 }
