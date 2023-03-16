f68ae08e010948fd046e6377ad0f5186c7c92b26
Sylvester Hesp <github@oisyn.nl>
2023-03-16 10:21:28 +0100

    Stripped some caps and extensions you dont need

--- build.rs
+++ build.rs
@@ -3,11 +3,11 @@ use spirv_builder::{MetadataPrintout, SpirvBuilder, Capability};
 fn main() {
     SpirvBuilder::new("shaders", "spirv-unknown-vulkan1.1")
     .print_metadata(MetadataPrintout::Full)
-    .capability(Capability::RuntimeDescriptorArray)
+    //.capability(Capability::RuntimeDescriptorArray)
     .capability(Capability::Int64)
-    .capability(Capability::RayQueryKHR)
-    .extension("SPV_EXT_descriptor_indexing")
-    .extension("SPV_KHR_ray_query")
+    //.capability(Capability::RayQueryKHR)
+    //.extension("SPV_EXT_descriptor_indexing")
+    //.extension("SPV_KHR_ray_query")
     .build()
     .unwrap(); 
 }
